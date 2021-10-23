961008641e231b5e96a2e3606f786962210a90e2
thejackforge <kimfidler@gmail.com>
2021-10-23 10:32:19 -0600

    second commit

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
+
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return <>
+    {/* <h1>{firstValue}</h1>
+    <h1>value : {secondValue}</h1> */}
+    {/* if(){console.log('hello world')}} */}
+    <h1>{text || 'john doe'}</h1>
+    <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+    {isError && <h1>Error...</h1>}
+    {isError ? <p>Error exists...</p> : <div>
+      <h2>There is no error</h2></div>}
+  </>;
 };
 
 export default ShortCircuit;
